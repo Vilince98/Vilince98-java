5c5e091df51c83d9291053f738c35d978ce2b1b9
vilince98-java <vilincep@gmail.com>
2023-02-17 00:37:45 +0530

    initial

---
+++ src/topsass/assi1.java
@@ -0,0 +1,20 @@
+package topsass;
+
+import java.util.Scanner;
+
+public class assi1 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int x, y, z;
+		
+		System.out.println("Enter first number: ");
+		x = sc.nextInt();
+		System.out.println("Enter Seconf number: ");
+		y = sc.nextInt();
+		System.out.println("Enter thirt number: ");
+		z = sc.nextInt();
+		
+		
+		
+	}
+}
